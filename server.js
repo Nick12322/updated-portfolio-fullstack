7c52ad3c0b0723b6d7ba17534d8da4d58d2d4d59
Nick Woodward <nickwoodward5@gmail.com>
2018-09-13 21:24:23 -0700

    test

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
+// var express = require("express");
+// var bodyParser = require("body-parser");
+// var path = require("path");
 
-var app = express();
-var PORT = 3000;
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "/index.html"));
-});
+// var app = express();
+// var PORT = 3000;
 
 
+// app.engine('html', require('ejs').renderFile);
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 
-
+// app.get("/", function(req, res) {
+//     res.render(path.join(__dirname, "./index.html"));
+// });
 
 
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-});
+// app.listen(PORT, function() {
+//     console.log("App listening on PORT " + PORT);
+// }); 
+
